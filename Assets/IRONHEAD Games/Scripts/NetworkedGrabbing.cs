764deede0339041288ba9c9e639134d3f69f970c
ElieHousseini1 <eliehousseini@postlight.com>
2022-03-27 10:46:54 +0300

    remote precedure calls for grabbing nerfgun

--- Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
+++ Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
@@ -8,6 +8,9 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
 {
     PhotonView m_photonView;
 
+    Rigidbody rb;
+    bool isBeingHeld = false;
+
     private void Awake(){
         m_photonView = GetComponent<PhotonView>();
     }
@@ -15,13 +18,21 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(isBeingHeld){
+            // Object is being grabbed
+            rb.isKinematic = true;
+            gameObject.layer = 11;
+        } else{
+            // Object is not being grabbed
+            rb.isKinematic = false;
+            gameObject.layer = 9;
+        }
     }
 
     private void TransferOwnership(){
@@ -30,14 +41,26 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
 
     public void OnSelectEntered(){
         Debug.Log("Grabbed");
+        m_photonView.RPC("StartNetworkGrabbing", RpcTarget.AllBuffered);
+
+        if(m_photonView.Owner == PhotonNetwork.LocalPlayer){
+            Debug.Log("We do not request the ownership. Already mine.");
+        } else {
         TransferOwnership();
+        }
     }
     public void onSelectExited(){
         Debug.Log("Released");
+        m_photonView.RPC("StopNetworkGrabbing", RpcTarget.AllBuffered);
     }
 
     public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
     {
+
+        if(targetView != m_photonView){
+            return;
+        }
+
         Debug.Log("Ownership Requested for: " + targetView.name + " from " + requestingPlayer.NickName);
         m_photonView.TransferOwnership(requestingPlayer);
     }
@@ -51,4 +74,14 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
     {
 
     }
+
+    [PunRPC]
+    public void StartNetworkGrabbing(){
+        isBeingHeld = true;
+    }
+    [PunRPC]
+    public void StopNetworkGrabbing(){
+        isBeingHeld = false;
+    }
+
 }
